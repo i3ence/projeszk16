af9c234bcd992c5c176f5c93e190c0bfd1281a01
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-02 01:00:46 +0200

    Added size to map

--- agario-client/src/main/java/client/model/map/Map.java
+++ agario-client/src/main/java/client/model/map/Map.java
@@ -6,20 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * A square-shaped map.
  * @author zoli-
  * @author yzsolt
  */
 public class Map {
 
-    List<MapObject> mapContent;
+    /** Size of the map's sides. */
+    int size;
     
+    List<MapObject> objects;
     
     /**
      * initialize empty map
      */
-    public Map() {
-        mapContent = new ArrayList<>();
+    public Map(int size) {
+        this.size = size;
+        this.objects = new ArrayList<>();
     }
     
     /**
@@ -28,11 +31,15 @@ public class Map {
      * @param newMap 
      */
     public void update(Map newMap) {
-        this.mapContent = newMap.mapContent;
+        this.objects = newMap.objects;
+    }
+    
+    public int getSize() {
+        return size;
     }
     
     public List<MapObject> getObjects() {
-        return mapContent;
+        return objects;
     }
     
 }
