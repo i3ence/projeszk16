f831f25ccf0f49adb8f8336a9f485510e7fb75b0
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-01 23:50:35 +0200

    Added an utility class for the client

---
+++ agario-client/src/main/java/client/Util.java
@@ -0,0 +1,40 @@
+
+package client;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+
+/**
+ * Various utility functions for the client.
+ * @author yzsolt
+ */
+public class Util {
+    
+    public static String getFileContents(String path) throws IOException {
+        
+        List<String> lines = Files.readAllLines(new File(path).toPath());
+        StringBuilder file_content = new StringBuilder();
+        
+        for (String line : lines) {
+            file_content.append(line);
+            file_content.append('\n');
+        }
+        
+        return file_content.toString();
+        
+    }
+    
+    public static String byteBufferToString(ByteBuffer byte_buffer) {
+        
+        byte[] byte_array = new byte[byte_buffer.limit()];
+        byte_buffer.get(byte_array);
+        
+        return new String(byte_array, Charset.forName("UTF-8"));
+        
+    }
+    
+}
