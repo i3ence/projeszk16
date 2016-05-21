b71d7f504630f977f3d4621c18eaf7e311092bef
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-21 23:04:50 +0200

    Added ConnectionError

---
+++ agario-client/src/main/java/common/communication/ConnectionError.java
@@ -0,0 +1,33 @@
+
+package common.communication;
+
+/**
+ *
+ * @author yzsolt
+ */
+public class ConnectionError extends Exception {
+    
+    public enum Type {
+        CONNECTION_FAIL,
+        IOSTREAM_FAIL,
+        ACKNOWLEDGMENT_FAIL,
+        SERVER_FULL
+    }
+    
+    private final Type type;
+    
+    public ConnectionError(Type type) {
+        this.type = type;
+    }
+    
+    public Type getType() {
+        return type;
+    }
+    
+    @Override
+    public String toString() {
+        // TODO: switch-case
+        return "";
+    }
+    
+}
