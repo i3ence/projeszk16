68ec129f0575787bb119d9a3a80ae97de317dd7e
Zsolt Bölöny <bolony.zsolt@gmail.com>
2016-05-22 00:06:27 +0200

    Switched to multi-project build: client, server and the common

---
+++ agario/client/src/main/java/client/NetworkHandler.java
@@ -0,0 +1,138 @@
+package client;
+
+import common.communication.ConnectionError;
+import common.communication.SimpleResponse;
+import common.communication.RequestImpl;
+import common.communication.JoinAcknowledgment;
+import common.communication.Request;
+import common.model.SimpleMapObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import common.communication.JoinResponse;
+
+/**
+ *
+ * @author hegym
+ */
+public class NetworkHandler {
+    
+    // communication
+    private static Socket client;
+    private static OutputStream outStream;
+    private static ObjectOutputStream objectOutStream;
+    private static InputStream inStream;
+    private static ObjectInputStream objectInStream;
+
+    // response
+    private static int id;
+    private static float mapSize;
+    
+    //init return values
+    public final static int SUCCESS = 0;
+    public final static int CONNECTION_FAIL = 1;
+    public final static int IOSTREAM_FAIL = 2;
+    public final static int ACKNOWLEDGMENT_FAIL = 3;
+    public final static int SERVER_FULL = 4;
+    
+    //singleton
+    private static NetworkHandler nethandler = new NetworkHandler();
+    
+    private NetworkHandler() {  }
+    
+    public static NetworkHandler getInstance( ) {
+      return nethandler;
+   }
+    
+    /**
+     * Initialize connection to the server.
+     * @param ipAddress IP Address of the server.
+     * @param portNumber Port to connect to.
+     * @param name Name of the player.
+     * @return JoinResponse
+     * @throws common.communication.ConnectionError
+     */
+    public static JoinResponse initConnection(String ipAddress, int portNumber, String name) throws ConnectionError {
+        
+        try { 
+            client = new Socket(ipAddress, portNumber); 
+        } catch (IOException e) { 
+            throw new ConnectionError(ConnectionError.Type.CONNECTION_FAIL); 
+        }
+
+        // init streams
+        try {
+            outStream = client.getOutputStream();
+            inStream = client.getInputStream();
+            objectOutStream = new ObjectOutputStream(outStream);
+            objectInStream = new ObjectInputStream(inStream);
+        } catch (IOException e) { 
+            throw new ConnectionError(ConnectionError.Type.IOSTREAM_FAIL); 
+        }
+
+        // acknowledgment from server
+        try {
+            
+            JoinResponse response = (JoinResponse)objectInStream.readObject();
+            
+            if (response.getStatus() == 0) {
+                
+                // accepted
+                mapSize = response.getMapSize();
+                id = response.getId();
+                JoinAcknowledgment joinAck = new JoinAcknowledgment(name);
+                objectOutStream.writeObject(joinAck);
+                objectOutStream.flush();
+                
+                return response;
+                
+            } else {
+                // rejected
+                throw new ConnectionError(ConnectionError.Type.SERVER_FULL); 
+            }
+            
+        } catch (ClassNotFoundException | IOException e) {
+            //Logger.getLogger(AgarioGame.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(e);
+            throw new ConnectionError(ConnectionError.Type.ACKNOWLEDGMENT_FAIL); 
+        }
+
+    }
+    
+    /**
+     * Send request to server, call this as much as possible.
+     * @param angle the angle where player cell is moving
+     * @param status Current status of the player.
+     */
+    public static void sendRequest(float angle, int status) {
+        Request request = new RequestImpl(angle, status);
+        try {
+            objectOutStream.writeObject(request);
+            objectOutStream.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(AgarioGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    /**
+     * Receive the simple response from server
+     * !! if response is not received, this will throw NPE. Need to investigate!
+     * @return server response
+     */
+    public static List<? super SimpleMapObject> handleSimpleResponse() {
+        SimpleResponse simpleResponse = null;
+        try {
+            simpleResponse = (SimpleResponse) objectInStream.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(AgarioGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return simpleResponse.getMapObjects();
+    }
+}
